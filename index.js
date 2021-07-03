cecf2aba77cc31abe4e29229f3c1bf5be2a25e61
yeungparkhay <yeungparkhay@gmail.com>
2021-07-03 08:18:16 +0100

    completed exercise 3.6

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+app.use(express.json())
+
+let persons = [
     {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "39-44-5323523"
+        id: 1,
+        name: "Arto Hellas",
+        number: "39-44-5323523"
     },
     {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "12-43-234345"
+        id: 2,
+        name: "Ada Lovelace",
+        number: "12-43-234345"
     },
     {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "39-23-6423122"
+        id: 3,
+        name: "Dan Abramov",
+        number: "39-23-6423122"
     },
     {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "123444"
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "123444"
     }
 ]
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.filter(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 app.get('/info', (request, response) => {
@@ -34,7 +46,47 @@ app.get('/info', (request, response) => {
         <br>
         <div>${new Date()}</br>
     `)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const generateId = () => {
+        let newId = Math.floor(Math.random() * 10000)
+        while (persons.map(person => Number(person.id)).filter(id => id === newId).length > 0) {
+            newId = Math.floor(Math.random() * 10000)
+            console.log("randomised")
+        }
+        return newId
+    }
     
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    if (persons.map(person => person.name).filter(name => name === body.name).length > 0) {
+        return response.status(409).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const newPerson = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(newPerson)
+
+    response.json(newPerson)
 })
 
 const PORT = 3001
