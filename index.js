7add4dc7b22c9596b04feacd82a2a7dd6412ddae
yeungparkhay <yeungparkhay@gmail.com>
2021-07-05 12:02:03 +0100

    completed exercise 38

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+app.use(morgan(function (tokens, req, res) {
+    if (req.method === 'POST') {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'), '-',
+            tokens['response-time'](req, res), 'ms',
+            JSON.stringify(req.body)
+        ].join(' ')
+    } else {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'), '-',
+            tokens['response-time'](req, res), 'ms',
+        ].join(' ')
+    }
+}))
 
 let persons = [
     {
